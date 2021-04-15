fcf5094e0fedd5454bf376f53f89f2ae9e54bc70
Lándori Dávid <info@honlapvarazslo.com>
2021-04-15 13:14:46 +0200

    🎨   Add utils

--- utils/index.js
+++ utils/index.js
@@ -4,3 +4,39 @@ export const setHttp = (link) => {
   }
   return link;
 };
+
+export const formatPrice = (price) => {
+  if (!price) return null;
+
+  const client = new Intl.NumberFormat("hu-HU", {
+    style: "currency",
+    currency: "HUF",
+    maximumFractionDigits: 0,
+    minimumFractionDigits: 0,
+  });
+
+  return client.format(price);
+};
+
+export const addSingleItemToFilter = (filterArray, name, value) => {
+  if (!value) return;
+
+  filterArray.push(`${name}: ${value}`);
+};
+
+export const addMultiItemToFilter = (filterArray, name, values) => {
+  if (values.length < 1) return;
+
+  const items = values.map((item) => `${name}: "${item}"`);
+  filterArray.push(items.join(" OR "));
+};
+
+export const addRangeItemToFilter = (filterArray, name, values) => {
+  if (values.min) {
+    filterArray.push(`${name} >= ${values.min}`);
+  }
+
+  if (values.max) {
+    filterArray.push(`${name} <= ${values.max}`);
+  }
+};
